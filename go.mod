eb264a19716edb225b61f0f7add95eaa5d13e9d8
pengyuanwang <wpy1174555847@outlook.com>
2024-02-19 10:08:59 +0800

    [2024-02-19] add ratelimit

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/juju/ratelimit v1.0.2 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible // indirect
 	github.com/lestrrat-go/strftime v1.0.6 // indirect
--- main.go
+++ main.go
@@ -10,8 +10,10 @@ package main
 
 import (
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/juju/ratelimit"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
@@ -29,13 +31,25 @@ func CORSMiddleware() gin.HandlerFunc {
 	}
 }
 
+func RateLimitMiddleware(fillInterval time.Duration, cap, quantum int64) gin.HandlerFunc {
+	bucket := ratelimit.NewBucketWithQuantum(fillInterval, cap, quantum)
+	return func(c *gin.Context) {
+		if bucket.TakeAvailable(1) < 1 {
+			c.AbortWithStatus(429)
+			return
+		}
+		c.Next()
+	}
+}
+
 func Init() *gin.Engine {
-	gin.SetMode(gin.ReleaseMode)
+	// gin.SetMode(gin.ReleaseMode)
 
 	r := gin.Default()
 	logger := NewLogger()
 	r.Use(LoggerMiddleware(logger))
 	r.Use(CORSMiddleware())
+	r.Use(RateLimitMiddleware(1*time.Second, 100, 50))
 
 	AddRouters(r, logger)
 
@@ -46,7 +60,7 @@ func main() {
 	r := Init()
 
 	if environment := os.Getenv("GIN_MODE"); environment == "release" {
-		r.Run(":80")
+		r.Run(":8000")
 	} else {
 		r.Run(SERVER_PORT)
 	}
